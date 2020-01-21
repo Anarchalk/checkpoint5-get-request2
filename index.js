0b07a245705ad43575f51c36a09c77d7f81f7a9a
Anar Chalkhaasuren <annachak9@gmail.com>
2020-01-20 23:10:05 -0500

    added if else statement for error message

--- index.js
+++ index.js
@@ -1,47 +1,39 @@
-function getDogImage (getValue) {
-     fetch('https://dog.ceo/api/breed/'+getValue+'/images/random')
+function getDogImage(getValue) {
+  fetch('https://dog.ceo/api/breed/' + getValue + '/images/random')
     .then(response => response.json())
-    .then(responseJson => console.log(displayResults(responseJson)))
-
+    .then(responseJson => {
+      if (responseJson.status == 'success') {
+        displayResults(responseJson)
+      }
+      else {
+        throw responseJson;
+      }
+    })
+    .catch(err => {
+      alert('Something went wrong. Try again. The breed might not be available.')
+    })
 }
-  
-
-// if (responseJson.status === "error") {
-//   displayErrorMessage(responseJson.message);
-// } else {
-//   displayResults(responseJson);
-// }
-// .catch(error => displayErrorMessage(error));
-// } else {
-// displayErrorMessage("please enter a breed");
-
-// //error message
-// function displayErrorMessage (errorMsg) {
-//   let errorStr = `<h3> ${errorMsg}</h3>`
-//   $( $('#js-display').html(errorStr)
-// }
-
 
 //display result
 function displayResults(responseJson) {
-    //replace the existing image with the new one
-    $('.results-img').replaceWith(
-      `<img src="${responseJson.message}" class="results-img">`)
-    //display the results section
-    $('.results').removeClass('hidden');
-  }
- 
-function eventListen() {
-    $('form').submit(event => {
-      event.preventDefault();
-      let getValue = document.getElementById("hariu").value;
-      console.log("testing",getValue);
-      getDogImage(getValue);
-      $('#hariu').val();
-    });
-  }
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`)
+  //display the results section
+  $('.results').removeClass('hidden');
+}
 
-$(function() {
-    console.log('App loaded! Waiting for submit!');
-    eventListen();
+function eventListen() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let getValue = document.getElementById("hariu").value;
+    console.log("testing", getValue);
+    getDogImage(getValue);
+    $('#hariu').val();
   });
+}
+
+$(function () {
+  console.log('App loaded! Waiting for submit!');
+  eventListen();
+});
